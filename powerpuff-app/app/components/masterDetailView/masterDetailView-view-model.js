897ea1de39918e29bed4a21dbc7ac4b3e4da7827
Svetozar Toskov <svetozar.toskov@telerik.com>
2016-06-13 20:52:42 +0000

    Changes made by Svetozar Toskov (svetozar.toskov@telerik.com).

--- powerpuff-app/app/components/masterDetailView/masterDetailView-view-model.js
+++ powerpuff-app/app/components/masterDetailView/masterDetailView-view-model.js
@@ -1,20 +1,11 @@
 'use strict';
 var ViewModel,
     Observable = require('data/observable').Observable;
-// additional requires
 
 ViewModel = new Observable({
-
     pageTitle: 'Quotes List',
-
     isLoading: false,
-    listItems: [],
-    // additional properties
-
+    listItems: []
 });
 
-// START_CUSTOM_CODE_masterDetailView
-// Add custom code here. For more information about custom code, see http://docs.telerik.com/platform/screenbuilder/troubleshooting/how-to-keep-custom-code-changes
-
-// END_CUSTOM_CODE_masterDetailView
 module.exports = ViewModel;
