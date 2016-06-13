38b26f1c36bc73f62196e2696e4a6c10a6e0fdc0
Aleksandar hitrov <aleksandar.hitrov@telerik.com>
2016-06-13 10:02:49 +0000

    accelerometer POC

--- powerpuff-app/app/components/homeView/homeView-view-model.js
+++ powerpuff-app/app/components/homeView/homeView-view-model.js
@@ -1,12 +1,29 @@
 'use strict';
 var ViewModel,
     Observable = require('data/observable').Observable;
+    //accelerometer = require("nativescript-accelerometer"),
+    //dialogs = require("ui/dialogs");
 // additional requires
 
 ViewModel = new Observable({
 
     pageTitle: 'Home View',
     // additional properties
+    
+    //init: function() {
+    //    var that = this;
+    //    accelerometer.startAccelerometerUpdates(function(data) {
+    //    dialogs.alert("x: " + data.x + "y: " + data.y + "z: " + data.z);});
+    //},
+    
+    //_accelerometerUpdates: function(data) {
+    //    var that = this;        
+    //    
+    //    dialogs.alert("Shaken x:" + data.x + " y:" + data.y + " z:" + data.z);
+    //    if (data.x + data.y + data.z > 10) {
+    //        dialogs.alert("Shaken not stirred!");
+    //    }
+    //}
 });
 
 // START_CUSTOM_CODE_homeView
