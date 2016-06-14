a4f33395511a43637360dad55df62533e1ee1517
Martin Bektchiev <martin.bektchiev@telerik.com>
2016-06-14 09:06:36 +0000

    Fix data-service.js errors

--- powerpuff-app/app/dataProviders/data-service.js
+++ powerpuff-app/app/dataProviders/data-service.js
@@ -1,4 +1,4 @@
-var dataProvider = require("../../dataProviders/powerpuffApp"),
+var dataProvider = require("./powerpuffApp"),
     TelerikBackendServices = require('../everlive/everlive.all.min');
 
 function generateSampleQuotes() {
@@ -8,22 +8,25 @@ function generateSampleQuotes() {
 }
 
 function getQuote(mood) {
-    return provider.businessLogic.invokeCloudFunction("GetQuote", {
+    return dataProvider.businessLogic.invokeCloudFunction("GetQuote", {
         mood: mood
     });
 }
 
 function getUserQuotes() {
-    return provider.Users.currentUser()
+    return dataProvider.Users.currentUser()
         .then(function (e) {
             var filter = new TelerikBackendServices.Query();
             filter.where().eq('Owner', e.result.Id);
+        	filter.expand({ "Quote": { "TargetTypeName": "Quotes", "Fields": { "Text": 1, "Author": 1 } } });
 
-            var data = provider.data('UserQuotes');
+            var data = dataProvider.data('UserQuotes');
             return data.get(filter);
         });
 }
 
 module.exports = {
-    generateSampleQuotes: generateSampleQuotes
+    generateSampleQuotes: generateSampleQuotes,
+    getUserQuotes: getUserQuotes,
+    getQuote: getQuote,
 };
